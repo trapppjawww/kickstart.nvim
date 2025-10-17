657036a95104ee299c44fb78c9886714f1863a55
tj <238725676+trapppjawww@users.noreply.github.com>
2025-10-17 11:22:12 -0700

    associating filetypes with prettierd

--- init.lua
+++ init.lua
@@ -770,6 +770,16 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        sass = { 'prettierd' },
+        json = { 'prettierd' },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        vue = { 'prettierd' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
